59fee4ad255b3790e2e29e7ac18a9bf9387649a6
Ivin benjamin <55826241+raswer4@users.noreply.github.com>
2021-02-21 23:05:57 +0100

    recycleView got a update

--- app/src/main/java/com/example/watchlist/sampledata/UserListFragment.kt
+++ app/src/main/java/com/example/watchlist/sampledata/UserListFragment.kt
@@ -3,40 +3,36 @@ package com.example.watchlist.sampledata
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.watchlist.*
-
-import com.example.watchlist.databinding.ActivityMainMenuBinding
 import com.example.watchlist.databinding.FragmentUserListBinding
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 
-class WatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
 class UserListFragment : Fragment() {
 
-
+    class WatchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     lateinit var binding: FragmentUserListBinding
     lateinit var button: Button
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    )= FragmentUserListBinding.inflate(inflater, container,false).run {
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    )= FragmentUserListBinding.inflate(inflater, container, false).run {
         binding = this
 
         val auth = FirebaseAuth.getInstance()
@@ -47,9 +43,10 @@ class UserListFragment : Fragment() {
                 .setLifecycleOwner(this@UserListFragment).build()
 
         val adapter = object: FirestoreRecyclerAdapter<Watch, WatchViewHolder>(options){
+
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WatchViewHolder {
                 val view : View = LayoutInflater.from(parent.context).inflate(
-                        android.R.layout.simple_list_item_2,
+                        R.layout.titles_layout,
                         parent,
                         false
                 )
@@ -67,6 +64,7 @@ class UserListFragment : Fragment() {
         userListRecyclerView.adapter = adapter
         userListRecyclerView.layoutManager = LinearLayoutManager(context)
 
+
         root
     }
 
@@ -75,9 +73,8 @@ class UserListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
             binding.addToUserList.setOnClickListener {
-                val i = Intent(activity,CreateWatchListActivity::class.java)
+                val i = Intent(activity, CreateWatchListActivity::class.java)
                 activity?.startActivity(i)
-
             }
 
     }
