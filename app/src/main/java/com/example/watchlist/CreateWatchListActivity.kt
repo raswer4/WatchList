a9172c45cdd672d076af6e7f8d3544d78b3c8765
abah19vo <70108548+abah19vo@users.noreply.github.com>
2021-02-22 16:59:16 +0100

    get an img from galary with its permisstions and upload to storage

--- app/src/main/java/com/example/watchlist/CreateWatchListActivity.kt
+++ app/src/main/java/com/example/watchlist/CreateWatchListActivity.kt
@@ -1,42 +1,114 @@
 package com.example.watchlist
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
-import com.example.watchlist.sampledata.UserListFragment
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.example.watchlist.sampledata.MainMenuActivity as MainMenuActivity1
+import androidx.appcompat.app.AppCompatActivity
+
 
 class CreateWatchListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
 
         setContentView(R.layout.activity_create_watch_list)
         val createWatchButton = this.findViewById<Button>(R.id.createWatchList)
+        val getImgBtn = this.findViewById<Button>(R.id.getImg)
 
-        createWatchButton.setOnClickListener(){
-            val watchTitle = this.findViewById<EditText>(R.id.createTitle).editableText.toString().trim()
-            val watchContent = this.findViewById<EditText>(R.id.createContent).editableText.toString().trim()
-            val watchDate = this.findViewById<EditText>(R.id.createDate).editableText.toString().trim()
-
-            val id = watchListRepository.addWatchList(watchTitle,watchContent,watchDate,"hello",this)
-            Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show()
-
-            val intent = Intent(this, WatchViewActivity::class.java).apply {
-                putExtra("id", id)
+        getImgBtn.setOnClickListener {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    val permissions  = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                }else{
+                    pickImgFromGallary()
+                }
+            }else{
+                pickImgFromGallary()
             }
-            startActivity(intent)
-            finish()
+
+        }
+        createWatchButton.setOnClickListener(){
+            addToWatchList()
+        }
 
 
+    }
+    companion object{
+        private val IMAGE_PICK_CODE = 1000
+        private val PERMISSION_CODE = 1001
+        private var imgToUpload = Uri.parse("android.resource://your.package.here/drawable/image_name")
+
+    }
+
+    private fun addToWatchList(){
+        val watchTitle = this.findViewById<EditText>(R.id.titleEditText).editableText.toString().trim()
+        val watchContent = this.findViewById<EditText>(R.id.contentEditText).editableText.toString().trim()
+        val watchDate = this.findViewById<EditText>(R.id.dateEditText).editableText.toString().trim()
+
+        val id = watchListRepository.addWatchList(
+            watchTitle,
+            watchContent,
+            watchDate,
+            imgToUpload,
+            this
+        )
+        Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this, WatchViewActivity::class.java).apply {
+            putExtra("id", id)
+        }
+        startActivity(intent)
+        finish()
+    }
+
+    private fun pickImgFromGallary(){
+        intent = Intent()
+        intent.type= "image/*"
+        intent.action= Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK){
+            imgToUpload = data?.data
+            val img = findViewById<ImageView>(R.id.watchImageView)
+            img.setImageURI(data?.data)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImgFromGallary()
+                } else {
+                    Toast.makeText(this, getString(R.string.permissionDenied), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/watchlist/WatchListRepository.kt
+++ app/src/main/java/com/example/watchlist/WatchListRepository.kt
@@ -1,10 +1,16 @@
 package com.example.watchlist
 import android.content.Context
+import android.net.Uri
+
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.net.URI
+import com.google.firebase.storage.ktx.storage
 import java.util.*
 
 val watchListRepository = WatchListRepository()
@@ -12,38 +18,43 @@ val watchListRepository = WatchListRepository()
 class WatchListRepository{
 
     private val watchLists = mutableListOf<Watch>()
+    private var storageRef = Firebase.storage.reference
 
-    fun addWatchList(title: String, content: String, date: String, img: String,context:Context): Int{
+    fun addWatchList(title: String, content: String, date: String, img: Uri,context:Context): Int{
         val id = when {
             watchLists.count() == 0 -> 1
             else -> watchLists.last().id+1
         }
-
+        Log.d("msg",img.toString())
         val database = FirebaseFirestore.getInstance()
         val auth = FirebaseAuth.getInstance()
         val currentUser = auth.currentUser
         val watch = HashMap<String, Any>()
-
-
-        watch.put("Title", title)
-        watch.put("Content", content)
-        watch.put("Date", date)
-
-        watchLists.add(Watch(
+        try {
+            watch.put("Title", title)
+            watch.put("Content", content)
+            watch.put("Date", date)
+            uploadImgToStorage(id,img)
+            watchLists.add(Watch(
                 id,
                 title,
                 content,
                 date,
-                img
-        ))
-
-        database.collection("users").document(currentUser!!.uid).collection("titles").document(title).set(watch)
+                img.toString()
+            ))
+            database.collection("users").document(currentUser!!.uid).collection("titles").document(title).set(watch)
                 .addOnSuccessListener {
                     Toast.makeText(context, "Success", Toast.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener{
+                    throw error("firestoreError")
                     Toast.makeText(context, "Failure LOL", Toast.LENGTH_SHORT).show()
                 }
+
+        }catch (e :  IllegalStateException){
+            throw e
+        }
+
         return id
     }
 
@@ -84,7 +95,7 @@ class WatchListRepository{
                         val title = document.data.getValue("Title") as String
                         val content = document.data.getValue("Content") as String
                         val date = document.data.getValue("Date") as String
-                        watchListRepository.addWatchList(title,content,date,"",context)
+                        watchListRepository.addWatchList(title,content,date, Uri.EMPTY,context)
 
                 }
             }.addOnSuccessListener {
@@ -93,6 +104,18 @@ class WatchListRepository{
                     Toast.makeText(context,"it did not work worked",Toast.LENGTH_SHORT).show()
                 }
 
+    }
+
+    fun uploadImgToStorage(id: Int ,imgUrl : Uri){
+        val user = Firebase.auth.currentUser
+        if(user != null) {
+            val imgPath = storageRef.child("images/${user.uid}/$id")
+            imgPath.putFile(imgUrl).addOnFailureListener{
+                throw error("error")
+            }
+        }else{
+            throw error("authError")
+        }
     }
 
 
